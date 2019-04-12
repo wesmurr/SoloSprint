c2e78f7b13355b9b848033a5275a8f2bfced9501
mhkend01 <lee.kendall@centre.edu>
2019-04-12 16:23:18 -0400

    added application package

---
+++ planner_networking/src/main/java/application/Main.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author lee.kendall
+ *
+ */
+public class Main extends Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
