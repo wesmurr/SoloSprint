269f60c967900654c77b89c75589b97f0ac198e5
TristanWilson <47950690+TristanWilson@users.noreply.github.com>
2019-04-13 16:53:41 -0400

    Edit PlanReadOnlyViewController.

--- planner_networking/src/main/java/planReadOnlyView/PlanReadOnlyViewController.java
+++ planner_networking/src/main/java/planReadOnlyView/PlanReadOnlyViewController.java
@@ -4,4 +4,18 @@ import planEditView.PlanEditViewController;
 
 public class PlanReadOnlyViewController extends PlanEditViewController
 {
+	public void deleteSecton()
+	{
+		return;
+	}
+	
+	public void addSection()
+	{
+		return;
+	}
+	
+	public void push()
+	{
+		return;
+	}
 }
