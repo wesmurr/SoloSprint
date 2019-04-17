513e9d43872065e6d05111b5dcb281e6b22ef008
mhkend01 <lee.kendall@centre.edu>
2019-04-17 16:04:59 -0400

    Messing around with testfx

--- planner_networking/src/test/java/software_masters/gui_test/ConnectServerTest.java
+++ planner_networking/src/test/java/software_masters/gui_test/ConnectServerTest.java
@@ -2,11 +2,23 @@ package software_masters.gui_test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit.ApplicationTest;
 
+import application.Main;
+
 class ConnectServerTest extends ApplicationTest {
 
+	@Before
+	public void setUpClass() throws Exception {
+		
+		ApplicationTest.launch(Main.class);
+	}
+	
+	
+	
+	
 	@Test
 	void test() { fail("Not yet implemented"); }
 
