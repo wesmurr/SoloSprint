775a186cfe1f559df4d3249ef5b9c20a928f67e8
liu.jiang <liu.jiang@cs-manticore>
2019-04-17 17:15:39 -0400

    before

--- planner_networking/src/test/java/software_masters/gui_test/ConnectServerTest.java
+++ planner_networking/src/test/java/software_masters/gui_test/ConnectServerTest.java
@@ -3,29 +3,32 @@ package software_masters.gui_test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.concurrent.TimeoutException;
-
-import org.junit.After;
 import org.junit.Before;
+import org.junit.After;
 import org.junit.jupiter.api.Test;
 import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit.ApplicationTest;
 
 import application.Main;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
+import static org.testfx.api.FxAssert.verifyThat;
 
 class ConnectServerTest extends ApplicationTest {
 
 	@Before
 	public void setUpClass() throws Exception {
 		
+		System.out.println("Got here!");
 		ApplicationTest.launch(Main.class);
 	}
 	
 	
 	@Override
 	public void start(Stage stage) throws Exception {
+		
 		stage.show();
 	}
 	
@@ -36,7 +39,17 @@ class ConnectServerTest extends ApplicationTest {
 		release(new MouseButton[] {});
 	}
 	
+	String IPLABEL_ID = "#ipLabel";
+	String PORTLABEL_ID ="#portLabel";
+	String IPFIELD_ID = "#ipField";
+	String PORTFIELD_ID ="#portField";
+	String CONNECTBUTTON_ID = "#connectButton";
+	
 	@Test
-	void test() { fail("Not yet implemented"); }
+	public void defaultValueTest()
+	{	
+		System.out.println("Test, here!");
+		verifyThat(IPFIELD_ID, (TextField field) -> {return field.getText().contains("IP Address:");});
+	}
 
 }
