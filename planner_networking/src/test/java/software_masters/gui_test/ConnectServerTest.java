5299c45afd100dff8f5ac2c398d1e77942c2da1e
wesley <wesley@jarvis-personal>
2019-04-17 08:54:42 -0400

    add testfx dependency

---
+++ planner_networking/src/test/java/software_masters/gui_test/ConnectServerTest.java
@@ -0,0 +1,13 @@
+package software_masters.gui_test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+class ConnectServerTest extends ApplicationTest {
+
+	@Test
+	void test() { fail("Not yet implemented"); }
+
+}
